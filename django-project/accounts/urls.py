5ed52c5367c831f23023074a88a9cab9cfd24396
BigBeom <beomjun789@gmail.com>
2024-05-20 12:29:16 +0900

    [BAK] 회원가입 RUD 수정 완료

--- django-project/accounts/urls.py
+++ django-project/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('<str:nickname>/', views.user_detail),
+  path('<str:username>/', views.user_detail),
 ]
--- django-project/accounts/views.py
+++ django-project/accounts/views.py
@@ -1,22 +1,18 @@
 from django.shortcuts import get_object_or_404, get_list_or_404
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import status
-from .serializers import CustomRegisterSerializer, CustomUserDetailSerializer
+from .serializers import CustomUserDetailSerializer
 from .models import User
-
-
 
 @api_view(['GET', 'DELETE', 'PUT'])
 @permission_classes([IsAuthenticated])
-def user_detail(request, nickname):
-    user = get_object_or_404(User, nickname=nickname)
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username)
 
     if request.method == 'GET':
-        serializer = CustomRegisterSerializer(user)
+        serializer = CustomUserDetailSerializer(user)
         # print(serializer.data)
         return Response(serializer.data)
     
@@ -25,11 +21,8 @@ def user_detail(request, nickname):
         return Response(status=status.HTTP_204_NO_CONTENT)
     
     elif request.method == "PUT":
-        # Exclude nickname from the request data if it exists
-        data = request.data.copy()
-        data.pop('nickname', None)
-        serializer = CustomRegisterSerializer(user, data=request.data, partial=True)
+        serializer = CustomUserDetailSerializer(user, data=request.data, partial=True)
+        # print(request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save(request=request)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-
