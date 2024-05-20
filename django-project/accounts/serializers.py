d0121d3ea926628993ec463fae3063dd25a66f1c
BigBeom <beomjun789@gmail.com>
2024-05-20 11:33:30 +0900

    [BAK] accounts앱 models 및 serializers를 ERD에 맞게 수정

--- django-project/accounts/serializers.py
+++ django-project/accounts/serializers.py
@@ -8,8 +8,10 @@ from .models import User
 UserModel = get_user_model()
 
 class CustomRegisterSerializer(RegisterSerializer):
-    nickname = serializers.CharField(required=True, allow_blank=False, max_length=10)
+    username = serializers.CharField(required=True, allow_blank=False, max_length=10)
     email = serializers.EmailField(required=False, allow_blank=True)
+    # lastname = serializers.CharField(required=False, allow_blank=True, max_length=10)
+    # firstname = serializers.CharField(required=False, allow_blank=True, max_length=10)
     gender = serializers.ChoiceField(required=False, allow_blank=True, choices=User.GENDER_CHOICES)
     age = serializers.IntegerField(required=False, max_value=99)
     birthday = serializers.DateField(required=False)
@@ -26,34 +28,21 @@ class CustomRegisterSerializer(RegisterSerializer):
             'username': self.validated_data.get('username', ''),
             'password1': self.validated_data.get('password1', ''),
             'email': self.validated_data.get('email', ''),
-            'nickname': self.validated_data.get('nickname', ''),
+            'lastname': self.validated_data.get('lastname', ''),
+            'firstname': self.validated_data.get('firstname', ''),
             'gender': self.validated_data.get('gender', ''),
             'age': self.validated_data.get('age', None),
             'birthday': self.validated_data.get('birthday', None),
-            'salary': self.validated_data.get('salary', None),
-            'asset': self.validated_data.get('asset', None),
-            'debt': self.validated_data.get('debt', None),
-            'deposit': self.validated_data.get('deposit', None),
-            'saving': self.validated_data.get('saving', None),
-            'is_staff': self.validated_data.get('is_staff', ''),
+            'salary': self.validated_data.get('salary', 0),
+            'asset': self.validated_data.get('asset', 0),
+            'debt': self.validated_data.get('debt', 0),
+            'deposit': self.validated_data.get('deposit', 0),
+            'saving': self.validated_data.get('saving', 0),
+            'credit_loan': self.validated_data.get('credit_loan', 0),
+            'is_staff': self.validated_data.get('is_staff', False),
             'profile_image': self.validated_data.get('profile_image', None),
         }
     
-    # def save(self, **kwargs):
-    #     request = self.context.get('request')
-    #     user = super().save(**kwargs)
-    #     if request:
-    #         user.profile.last_updated_by = request.user
-    #     user.save()
-    #     return user
-
-    # def save(self, request=None, **kwargs):
-    #     # Custom save logic that can utilize the request
-    #     user = super().save(request=request)
-    #     if request:
-    #         user.profile.last_updated_by = request.user
-    #     user.save()
-    #     return user
 
 class CustomUserDetailSerializer(UserDetailsSerializer):
     class Meta:
@@ -66,12 +55,10 @@ class CustomUserDetailSerializer(UserDetailsSerializer):
             extra_fields.append(UserModel.USERNAME_FIELD)
         if hasattr(UserModel, 'EMAIL_FIELD'):
             extra_fields.append(UserModel.EMAIL_FIELD)
-        if hasattr(UserModel, 'first_name'):
-            extra_fields.append('first_name')
         if hasattr(UserModel, 'last_name'):
             extra_fields.append('last_name')
-        if hasattr(UserModel, 'nickname'):
-            extra_fields.append('nickname')
+        if hasattr(UserModel, 'first_name'):
+            extra_fields.append('first_name')
         if hasattr(UserModel, 'gender'):
             extra_fields.append('gender')
         if hasattr(UserModel, 'age'):
@@ -84,8 +71,16 @@ class CustomUserDetailSerializer(UserDetailsSerializer):
             extra_fields.append('asset')
         if hasattr(UserModel, 'debt'):
             extra_fields.append('debt')
+        if hasattr(UserModel, 'deposit'):
+            extra_fields.append('deposit')
+        if hasattr(UserModel, 'saving'):
+            extra_fields.append('saving')
+        if hasattr(UserModel, 'credit_loan'):
+            extra_fields.append('credit_loan')
+        if hasattr(UserModel, 'is_staff'):
+            extra_fields.append('is_staff')
         if hasattr(UserModel, 'profile_image'):
             extra_fields.append('profile_image')
         model = UserModel
         fields = ('pk', *extra_fields)
-        read_only_fields = ('email', 'nickname')
+        read_only_fields = ('email',)
