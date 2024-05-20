1f6ab3834c29c4dce01b77836fb975fac3ae0f98
BigBeom <beomjun789@gmail.com>
2024-05-20 17:58:31 +0900

    [FNT] 홈페이지 게시판 CR 기능 구현 완료

--- vue-project/src/views/CreateView.vue
+++ vue-project/src/views/CreateView.vue
@@ -3,6 +3,10 @@
     <h1>게시글 작성</h1>
     <form @submit.prevent="createArticle">
       <div>
+        <label for="category">카테고리 : </label>
+        <input type="text" v-model.trim="category" id="category">
+      </div>
+      <div>
         <label for="title">제목 : </label>
         <input type="text" v-model.trim="title" id="title">
       </div>
@@ -22,6 +26,7 @@ import { useUserInfoStore } from '@/stores/userinfo'
 import { useRouter } from 'vue-router'
 
 const store = useUserInfoStore()
+const category = ref(null)
 const title = ref(null)
 const content = ref(null)
 const router = useRouter()
@@ -31,6 +36,7 @@ const createArticle = function () {
     method: 'post',
     url: `${store.API_URL}/articles/`,
     data: {
+      category: category.value,
       title: title.value,
       content: content.value
     },
@@ -40,7 +46,7 @@ const createArticle = function () {
   })
     .then((response) => {
       // console.log(response.data)
-      router.push({ name: 'CommunityView' })
+      router.push({ name: 'communityview' })
     })
     .catch((error) => {
       console.log(error)
